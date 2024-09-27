9cdd91d2ed1ee2da3650a60aaaed0323e81c66ca
jihwankim97 <cool102476@naver.com>
2024-09-27 23:11:01 +0900

    feat/config-test
    설정파일 캐시옵션 추가
    확장변수 사용
    클래스가 아닌 main.ts에서 환경변수 사용

--- config-test/src/app.module.ts
+++ config-test/src/app.module.ts
@@ -3,7 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { WeatherModule } from './weather/weather.module';
-import config from './configs/config' 
+import config from './configs/config';
 
 console.log('env : ' + process.env.NODE_ENV);
 console.log('current working directory : ' + process.cwd());
@@ -11,6 +11,8 @@ console.log('current working directory : ' + process.cwd());
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      cache: true,
+      expandVariables: true,
       envFilePath: `${process.cwd()}/envs/${process.env.NODE_ENV}.env`,
       load: [config],
     }),
