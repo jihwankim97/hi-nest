325ad3496beb26d1c5e5b93162856d0c79a9f16f
jihwankim97 <cool102476@naver.com>
2024-09-27 17:48:17 +0900

    [s3] 커스텀 설정파일 테스트

--- config-test/src/app.module.ts
+++ config-test/src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { WeatherModule } from './weather/weather.module';
+import config from './configs/config' 
 
 console.log('env : ' + process.env.NODE_ENV);
 console.log('current working directory : ' + process.cwd());
@@ -11,6 +12,7 @@ console.log('current working directory : ' + process.cwd());
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: `${process.cwd()}/envs/${process.env.NODE_ENV}.env`,
+      load: [config],
     }),
     WeatherModule,
   ],
